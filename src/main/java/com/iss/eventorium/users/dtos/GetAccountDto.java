7fc74e83d8780f2246145de62c0628d30559d7eb
Ana Šinik <sinikana7@gmail.com>
2024-11-25 00:07:45 +0100

    feat: add REST controller methods for registration and sending activation link

---
+++ src/main/java/com/iss/eventorium/users/dtos/GetAccountDto.java
@@ -0,0 +1,24 @@
+package com.iss.eventorium.users.dtos;
+
+
+import com.iss.eventorium.users.models.City;
+import com.iss.eventorium.users.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetAccountDto {
+    private Long id;
+    private String email;
+    private String name;
+    private String lastname;
+    private String phoneNumber;
+    private String address;
+    private City city;
+    private Role role;
+}
---
+++ src/main/java/com/iss/eventorium/users/dtos/RegistrationRequestDto.java
@@ -0,0 +1,23 @@
+package com.iss.eventorium.users.dtos;
+
+import com.iss.eventorium.users.models.City;
+import com.iss.eventorium.users.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationRequestDto {
+    private String email;
+    private String password;
+    private String name;
+    private String lastname;
+    private String phoneNumber;
+    private String address;
+    private City city;
+    private Role role;
+}
