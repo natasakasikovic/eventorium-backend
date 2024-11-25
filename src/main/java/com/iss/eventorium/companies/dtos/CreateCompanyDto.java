331ba6acfb1dbf52e50d01abdf4aff770ece20ae
Ana Šinik <sinikana7@gmail.com>
2024-11-25 18:20:28 +0100

    fix: add Lombok annotations to CreateCompanyDto class

--- src/main/java/com/iss/eventorium/companies/dtos/CreateCompanyDto.java
+++ src/main/java/com/iss/eventorium/companies/dtos/CreateCompanyDto.java
@@ -1,10 +1,18 @@
 package com.iss.eventorium.companies.dtos;
 
 import com.iss.eventorium.users.models.City;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Time;
 import java.util.ArrayList;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCompanyDto {
     private String name;
     private String address;
