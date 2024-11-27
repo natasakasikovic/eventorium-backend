8dcff41b978395f5b32f4f3f03b55490e91298d8
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-11-27 16:20:18 +0100

    feat: Implement endpoints for report management in ReportController

---
+++ src/main/java/com/iss/eventorium/users/dtos/ReportResponseDto.java
@@ -0,0 +1,17 @@
+package com.iss.eventorium.users.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportResponseDto {
+    private Long id;
+    private String reason;
+    private Long respondentId;
+    private Long complainantId;
+}
