ecf9b3613ad1a2bfb7c4159166a048e9f8a1e252
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-12-05 01:01:32 +0100

    chore: add review annotations and sample data

--- src/main/java/com/iss/eventorium/interaction/models/Review.java
+++ src/main/java/com/iss/eventorium/interaction/models/Review.java
@@ -2,6 +2,7 @@ package com.iss.eventorium.interaction.models;
 
 import com.iss.eventorium.shared.models.Status;
 import com.iss.eventorium.user.models.Person;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,11 +12,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "reviews")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column (nullable = false)
     private LocalDateTime creationDate;
+
+    @Column (nullable = false)
     private Integer rating;
+
+    @Column (nullable = false)
     private String feedback;
+
+    @Enumerated (EnumType.STRING)
     private Status status;
-    private Person person;
+
+//    private Person person;
 }
