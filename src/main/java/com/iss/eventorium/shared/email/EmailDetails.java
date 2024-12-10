1412dd285b09a3cb675e00b20310ad1156bac32a
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-12-10 18:32:38 +0100

    feat: necessary email sending functionalities have been implemented
    Since this method in EmailService will be called from other services, controller for now is not implemented

---
+++ src/main/java/com/iss/eventorium/shared/email/EmailDetails.java
@@ -0,0 +1,14 @@
+package com.iss.eventorium.shared.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+}
---
+++ src/main/java/com/iss/eventorium/shared/email/EmailService.java
@@ -0,0 +1,32 @@
+package com.iss.eventorium.shared.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}") private String sender;
+
+    public String sendSimpleMail(EmailDetails details) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setText(details.getMsgBody());
+            mailMessage.setSubject(details.getSubject());
+
+            javaMailSender.send(mailMessage);
+            return "Mail Sent Successfully..."; // NOTE: for now, it will stay this way
+        }
+        catch (Exception e) {
+            return "Error while Sending Mail " + e.getMessage();
+        }
+    }
+
+}
