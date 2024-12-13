bffc11af3f09249e02694ad16a618bdbed46254f
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-12-13 21:53:29 +0100

    feat: Add product suggestions for budget

--- src/main/java/com/iss/eventorium/solution/repositories/ProductRepository.java
+++ src/main/java/com/iss/eventorium/solution/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.iss.eventorium.solution.repositories;
 
+import com.iss.eventorium.solution.dtos.products.ProductSummaryResponseDto;
 import com.iss.eventorium.solution.models.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p JOIN p.reviews r WHERE p.status = 'ACCEPTED' GROUP BY p.id ORDER BY AVG(r.rating) DESC")
     List<Product> findTopFiveProducts(Pageable pageable);
+
+    @Query("SELECT p FROM Product p JOIN p.category c WHERE c.id = :categoryId AND p.price <= :price ORDER BY p.price DESC")
+    List<Product> getBudgetSuggestions(Long categoryId, Double price);
 }
