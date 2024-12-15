c4945c79382703f293e3857486b239d4f79bb3eb
Ana Šinik <sinikana7@gmail.com>
2024-12-15 18:34:24 +0100

    refactor: set organizer on event creation

---
+++ src/main/java/com/iss/eventorium/user/services/AuthService.java
@@ -0,0 +1,30 @@
+package com.iss.eventorium.user.services;
+
+import com.iss.eventorium.user.models.User;
+import com.iss.eventorium.user.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                return userRepository.findByEmail(((UserDetails) principal).getUsername()).orElse(null);
+            }
+        }
+        return null;
+    }
+}
