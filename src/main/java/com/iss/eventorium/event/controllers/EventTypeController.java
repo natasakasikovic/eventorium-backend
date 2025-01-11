2a796e18e3d07169368321d6366e3dde9b91d936
Ana Šinik <sinikana7@gmail.com>
2025-01-11 23:12:13 +0100

    chore: remove unused methods

--- src/main/java/com/iss/eventorium/event/controllers/EventTypeController.java
+++ src/main/java/com/iss/eventorium/event/controllers/EventTypeController.java
@@ -3,10 +3,8 @@ package com.iss.eventorium.event.controllers;
 import com.iss.eventorium.event.dtos.eventtype.EventTypeRequestDto;
 import com.iss.eventorium.event.dtos.eventtype.EventTypeResponseDto;
 import com.iss.eventorium.event.services.EventTypeService;
-import com.iss.eventorium.shared.utils.PagedResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,11 +21,6 @@ public class EventTypeController {
     @GetMapping("/all")
     public ResponseEntity<List<EventTypeResponseDto>> getEventTypes() {
         return ResponseEntity.ok(eventTypeService.getEventTypes());
-    }
-
-    @GetMapping()
-    public ResponseEntity<PagedResponse<EventTypeResponseDto>> getEventTypesPaged(Pageable pageable) {
-        return ResponseEntity.ok(eventTypeService.getEventTypesPaged(pageable));
     }
 
     @GetMapping("/{id}")
