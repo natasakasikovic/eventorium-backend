0ac60c641b6354a4716e135d3008644a3fe45d71
Ana Šinik <sinikana7@gmail.com>
2025-01-25 15:37:00 +0100

    feat: add endpoints for managing favorite events

---
+++ src/main/java/com/iss/eventorium/event/controllers/AccountEventController.java
@@ -0,0 +1,31 @@
+package com.iss.eventorium.event.controllers;
+
+import com.iss.eventorium.event.services.AccountEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/account/events")
+@RequiredArgsConstructor
+public class AccountEventController {
+    private final AccountEventService service;
+
+    @PostMapping("/favourites/{id}")
+    public ResponseEntity<Void> addFavouriteEvent(@PathVariable Long id) {
+        service.addFavouriteEvent(id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/favourites/{id}")
+    public ResponseEntity<Void> removeFavouriteEvent(@PathVariable Long id) {
+        service.removeFavouriteEvent(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/favourites/{id}")
+    public ResponseEntity<Boolean> isFavouriteEvent(@PathVariable Long id) {
+        return ResponseEntity.ok(service.isFavouriteEvent(id));
+    }
+}
