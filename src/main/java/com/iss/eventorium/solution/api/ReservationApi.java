f61d6ed225da442249b4d57c85725a0ddbbe679b
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2025-06-16 02:29:48 +0200

    docs: add documentation for reservations

---
+++ src/main/java/com/iss/eventorium/solution/api/ReservationApi.java
@@ -0,0 +1,198 @@
+package com.iss.eventorium.solution.api;
+
+import com.iss.eventorium.solution.dtos.services.CalendarReservationDto;
+import com.iss.eventorium.solution.dtos.services.ReservationRequestDto;
+import com.iss.eventorium.solution.dtos.services.ReservationResponseDto;
+import com.iss.eventorium.solution.dtos.services.UpdateReservationStatusRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Tag(name = "Reservations")
+public interface ReservationApi {
+
+    @Operation(
+            summary = "Creates a reservation for service",
+            description =
+                    """
+                    Creates a reservation for a specific service under a specific event and sends a confirmation email
+                    to the user who made the reservation.
+                    Requires authentication and ORGANIZER authority.
+                    Only users with the `ORGANIZER` authority can access this endpoint.
+                    """,
+            security = { @SecurityRequirement(name="bearerAuth") },
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Reservation successfully created", useReturnTypeSchema = true),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Validation error - missing or invalid fields",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Missing Starting Time",
+                                                    value = "{ \"startingTime\": \"Starting time is required!\" }"
+                                            )
+                                    }
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized - invalid or missing token"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden - not enough permissions"),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Event or service not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Event Not Found",
+                                                    value = "{ \"error\": \"Event not found\" }"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "Service Not Found",
+                                                    value = "{ \"error\": \"Service not found\" }"
+                                            )
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Reservation conflict detected - overlapping time slot",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Overlapping Reservation Conflict",
+                                                    value = "{\"error\": \"The selected time slot for this service is already occupied. Please choose a different time.}"
+                                            )
+                                    }
+                            )
+                    )
+                    ,
+                    @ApiResponse(
+                            responseCode = "502",
+                            description = "Failed to send confirmation email",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Email Failure",
+                                                    value = "{ \"error\": \"Failed to send confirmation email.\" }"
+                                            )
+                                    }
+                            )
+                    )
+            }
+
+    )
+    ResponseEntity<Void> createReservation(
+            @Parameter(description = "Reservation request body with staring, ending time and planned amount", required = true)
+            @RequestBody ReservationRequestDto reservation,
+
+            @Parameter(description = "ID of the event for which the reservation is being created", required = true, example = "42")
+            Long eventId,
+
+            @Parameter(description = "ID of the service to be reserved", required = true, example = "7")
+            Long serviceId
+    );
+
+
+    @Operation(
+            summary = "Get accepted reservations for current provider",
+            description = """
+            Returns a list of accepted reservations for the currently authenticated provider. Used for calendar display."
+            Requires authentication and PROVIDER authority.
+            Only users with the `PROVIDER` authority can access this endpoint.""",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of accepted reservations",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = CalendarReservationDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - user is not authenticated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden - authenticated user is not a provider"
+                    )
+            }
+    )
+    ResponseEntity<List<CalendarReservationDto>> getProviderReservations();
+
+
+    @Operation(
+            summary = "Get pending reservations for upcoming events",
+            description = """
+                    Returns a list of pending reservations for the currently authenticated provider, limited to future (upcoming) events.
+                    Requires authentication and PROVIDER authority.
+                    Only users with the `PROVIDER` authority can access this endpoint.""",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of pending reservations",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ReservationResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - user is not authenticated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden - authenticated user is not a provider"
+                    )
+            }
+    )
+    ResponseEntity<List<ReservationResponseDto>> getPendingReservations();
+
+
+    @Operation(
+            summary = "Update reservation status",
+            description = "Updates the status of an existing reservation. ONLY THE AUTHENTICATED PROVIDER who owns the reservation can perform this operation.",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Reservation status successfully updated",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponseDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Reservation not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = @ExampleObject(
+                                            name = "Reservation Not Found",
+                                            value = "{ \"error\": \"Reservation with given ID not found\" }"
+                                    )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized - user is not authenticated"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden - authenticated user is not the provider who owns this reservation")
+            }
+    )
+    ResponseEntity<ReservationResponseDto> updateReservation(
+            @Parameter(description = "ID of the reservation to be updated", required = true)
+            Long id,
+
+            @Parameter(description = "New status to update the reservation", required = true)
+            @RequestBody UpdateReservationStatusRequestDto request);
+}
