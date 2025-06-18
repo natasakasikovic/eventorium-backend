1aeda3d88d87e2977a4f352c69d5e548bbb44063
Ana Šinik <sinikana7@gmail.com>
2025-06-18 18:21:22 +0200

    docs: add UserApi

--- src/main/java/com/iss/eventorium/user/exceptions/InvalidOldPasswordException.java
+++ src/main/java/com/iss/eventorium/user/exceptions/InvalidOldPasswordException.java
@@ -1,6 +1,6 @@
 package com.iss.eventorium.user.exceptions;
 
-public class InvalidOldPasswordException extends Exception {
+public class InvalidOldPasswordException extends RuntimeException {
 
     public InvalidOldPasswordException(String message) {
         super(message);
